7053d416a64ad58b1a970261d9f4250b1aeb2102
SirZemar <eduardo.w.marinho@gmail.com>
2023-06-15 12:20:33 +0100

    first commit
    - dev dependencies eslint, prettier and tsnd for easier compilation
    - prod dependencies typescript, express and ejs
    - create web server with configurable port and host

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	semi: true,
+	trailingComma:
+		"all",
+	printWidth: 65,
+	quoteProps:
+		"consistent",
+	singleQuote: false,
+	tabWidth: 2,
+	useTabs: true,
+	bracketSpacing: true,
+	arrowParens:
+		"always",
+	proseWrap:
+		"preserve",
+	endOfLine: "lf",
+};
