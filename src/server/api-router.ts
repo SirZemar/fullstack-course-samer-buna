1b423e5ed5ce0898ae753b223aa335b3e5f15f5e
SirZemar <eduardo.w.marinho@gmail.com>
2023-06-22 11:05:32 +0100

    get contests data

--- src/server/api-router.ts
+++ src/server/api-router.ts
@@ -1,15 +1,38 @@
 import express from "express";
 import cors from "cors";
 
-import testData from "../test-data.json";
+import { connectClient } from "./db";
+
+// import testData from "../test-data.json";
+
 const router = express.Router();
 
 router.use(cors());
-router.get("/contests", (req, res) => {
-	// get data from mongodb
-	res.send({ contests: testData });
+router.get("/contests", async (req, res) => {
+	const client = await connectClient();
+
+	const contests = await client
+		.collection("contests")
+		.find()
+		.project({
+			id: 1,
+			categoryName: 1,
+			contestName: 1,
+			_id: 0,
+		})
+		.toArray();
+
+	res.send({ contests });
 });
 
-// router.get("/contest");
+router.get("/contest/:contestId", async (req, res) => {
+	const client = await connectClient();
+
+	const contest = await client
+		.collection("contests")
+		.find({ id: req.params.contestId });
+
+	req.send({ contest });
+});
 
 export default router;
